48fa8a653b04844f36b62cee75911afc4c9c8f6f
Derek Johnson <derekmj74@gmail.com>
2014-08-12 13:51:24 +0100

    try using noscript and meta refresh

---
+++ meta.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+	<head>
+		<meta charset="utf-8">
+		<title>noscript</title>
+		<noscript>
+		<meta http-equiv="refresh" content="0;index.html">
+		</noscript>
+	</head>
+
+	<body>
+		<p>This is meta.php</p>
+	</body>
+</html>
